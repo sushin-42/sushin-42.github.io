249b347909c2c58214b49af8c6e90d754ef7c6b1
ssb0498 <newssb1@naver.com>
2021-12-12 17:57:25 +0900

    gatsby-plugin-manifest test nbb_icon.png

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,13 +9,13 @@ const Layout = ({ location, title, children }) => {
   if (isRootPath) {
     header = (
       <h1 className="main-heading">
-        <Link to="/">{title}</Link>
+        <Link to="/">DevSushin</Link>
       </h1>
     )
   } else {
     header = (
       <Link className="header-link-home" to="/">
-        {title}
+        DevSushin
       </Link>
     )
   }
